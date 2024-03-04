a4bd7e53336cdd0814ae063965b98259807f1cdd
rezki bimo <rezkibimo@users.noreply.github.com>
2024-03-04 22:10:24 +1100

    updated todo
    styling update

--- components/TodoList.js
+++ components/TodoList.js
@@ -32,27 +32,30 @@ const TodoList = () => {
     };
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>TODO App</h1>
-                <div className="todo-input">
-                    <input
-                        type="text"
-                        placeholder="Add a new task"
-                        value={task}
-                        onChange={(e) => setTask(e.target.value)}
-                    />
-                    <button onClick={handleAddTodo}>Add</button>
-                </div>
-                <ul className="todo-list">
-                    {todos.map((todo, index) => (
-                        <li key={index}>
-                            {todo}
-                            <button onClick={() => handleRemoveTodo(index)}>Remove</button>
-                        </li>
-                    ))}
-                </ul>
-            </header>
+        <div>
+            <h1>TODO List</h1>
+            <div className="flex gap-1 mb-6">
+                <input
+                    type="text"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Add a new task"
+                    value={task}
+                    onChange={(e) => setTask(e.target.value)}
+                />
+                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={handleAddTodo}>Add</button>
+            </div>
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <tbody>
+                    <tr>
+                        {todos.map((todo, index) => (
+                            <td key={index} className="flex justify-between odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 px-6 py-4">
+                                {todo}
+                                <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onClick={() => handleRemoveTodo(index)}>Remove</button>
+                            </td>
+                        ))}
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
