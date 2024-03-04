627315103760775fae79e014a93221d8d0f94af9
rezki bimo <rezkibimo@users.noreply.github.com>
2024-03-04 19:32:20 +1100

    quote widget
    quote widget basic functionality and basic dashboard structure

---
+++ components/WeatherWidget.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+const WeatherWidget = () => {
+return(
+    <div className="flex-1">
+        <h1>Weather Widget HERE</h1>
+        {/* <Image
+    //       className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+    //       src="/next.svg"
+    //       alt="Next.js Logo"
+    //       width={180}
+    //       height={37}
+    //       priority
+    //     /> */}
+    </div>
+);
+}
+
+
+export default WeatherWidget
