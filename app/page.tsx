58965ef35d8b05258d45c9080983e4b651543ee3
rezki bimo <rezkibimo@users.noreply.github.com>
2024-03-04 22:21:01 +1100

    layout styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import TodoList from "@/components/TodoList.js";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
+    <main className="container mx-auto px-4 py-8 sm:px-6 sm:py-12 md:px-8 md:py-16 lg:px-10 lg:py-20 flex min-h-screen">
       <div className="max-w-[50rem] flex flex-col mx-auto size-full">
         <div className="flex">
           <WeatherWidget />
--- components/TodoList.js
+++ components/TodoList.js
@@ -44,11 +44,11 @@ const TodoList = () => {
                 />
                 <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={handleAddTodo}>Add</button>
             </div>
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <tbody>
                     <tr>
                         {todos.map((todo, index) => (
-                            <td key={index} className="flex justify-between odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 px-6 py-4">
+                            <td key={index} className="flex justify-between items-center odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 px-6 py-4">
                                 {todo}
                                 <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onClick={() => handleRemoveTodo(index)}>Remove</button>
                             </td>
