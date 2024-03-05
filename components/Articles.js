44f23ca0a033310b70c7ecaafbc90c9725228c07
rezki bimo <rezkibimo@users.noreply.github.com>
2024-03-05 15:37:08 +1100

    added missing article key for loop
    also upgrade github workflow version

--- components/Articles.js
+++ components/Articles.js
@@ -27,6 +27,7 @@ function Articles() {
             <div
                 class="grid grid-flow-row md:gap-4 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 {data.map((article) => (<div
+                key={article.id}
                     class="my-8 rounded shadow-lg shadow-gray-200 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-300 hover:-translate-y-1">
                     <a href={article.url} class="cursor-pointer">
                         <figure>
