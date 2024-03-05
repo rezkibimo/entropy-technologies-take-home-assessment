2e8db72ff28fee9dc8487687d80ed8bedddf33c7
rezki bimo <rezkibimo@users.noreply.github.com>
2024-03-05 16:47:55 +1100

    todo list design update

--- components/TodoList.js
+++ components/TodoList.js
@@ -43,12 +43,13 @@ const TodoList = () => {
                     type="text"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Add a new task"
+                    maxLength={100}
                     value={task}
                     onChange={(e) => setTask(e.target.value)}
                 />
                 <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={handleAddTodo}>Add</button>
             </div>
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <table className="table-auto w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <tbody>
                     {todos.map((todo, index) => (
                         <tr key={index} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 pl-6 py-4">
@@ -58,7 +59,7 @@ const TodoList = () => {
                                     checked={todo.completed}
                                     onChange={() => handleToggleComplete(index)}
                                 />
-                                <span className={`mr-auto pl-4 ${todo.completed ? 'line-through' : ''}`}> {todo.task}</span>
+                                <span className={`mr-auto px-4 ${todo.completed ? 'line-through' : ''}`} style={{ wordBreak: 'break-all' }}>{todo.task}</span>
                                 <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onClick={() => handleRemoveTodo(index)}>Remove</button>
                             </td>
                         </tr>
