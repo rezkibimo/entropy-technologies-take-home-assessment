c5e7f1df5dfbec3399c1474d77352e28ee248101
rezki bimo <rezkibimo@users.noreply.github.com>
2024-03-05 14:12:15 +1100

    updated unit test

--- tests/todolist.spec.ts
+++ tests/todolist.spec.ts
@@ -1,15 +1,22 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('todo list', async ({ page }) => {
+test("add and remove item to todo list", async ({ page }) => {
   // Navigate to the todo list website
-  await page.goto('https://entropy-technologies-take-home-assessment.vercel.app/');
+  await page.goto(
+    "https://entropy-technologies-take-home-assessment.vercel.app/"
+  );
 
-  // Add a new todo
-  const newTodoInput = await page.locator('input[placeholder="Add a new task"]');
-  await newTodoInput.fill('New Todo');
+  // Add item
+  const newTodoInput = await page.locator(
+    'input[placeholder="Add a new task"]'
+  );
+  await newTodoInput.fill("New Todo");
   const addButton = await page.locator('button:has-text("Add")');
   await addButton.click();
-  
-  // Optionally, you can add a wait to see the addition visually
+
   await page.waitForTimeout(2000);
+
+  // Remove item
+  const removeButton = await page.locator('button:has-text("Remove")');
+  await removeButton.click();
 });
