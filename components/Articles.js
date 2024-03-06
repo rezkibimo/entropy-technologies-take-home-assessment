770ef4a18dbfe829a91486f9f15876d4dda51056
rezki bimo <rezkibimo@users.noreply.github.com>
2024-03-06 16:20:33 +1100

    removed dark mode

--- components/Articles.js
+++ components/Articles.js
@@ -28,12 +28,12 @@ function Articles() {
                 className="grid grid-flow-row md:gap-4 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 {data.map((article) => (<div
                     key={article.id}
-                    className="my-8 rounded shadow-lg shadow-gray-200 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-300 hover:-translate-y-1">
+                    className="my-8 rounded shadow-lg shadow-gray-200 duration-300 hover:-translate-y-1">
                     <a href={article.url} className="cursor-pointer">
                         <figure>
                             <img src={article.thumbnailUrl} className="rounded-t h-72 w-full object-cover" />
                             <figcaption className="p-4">
-                                <p className="text-lg mb-4 font-bold leading-relaxed text-gray-800 dark:text-gray-300">
+                                <p className="text-lg mb-4 font-bold leading-relaxed text-gray-800">
                                     {article.title}
                                 </p>
                             </figcaption>
--- components/TodoList.js
+++ components/TodoList.js
@@ -41,18 +41,18 @@ const TodoList = () => {
             <div className="flex lg:flex-row flex-col gap-1 mb-6">
                 <input
                     type="text"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                     placeholder="Add a new task"
                     maxLength={100}
                     value={task}
                     onChange={(e) => setTask(e.target.value)}
                 />
-                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={handleAddTodo}>Add</button>
+                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none" onClick={handleAddTodo}>Add</button>
             </div>
-            <table className="table-auto w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <table className="table-auto w-full text-sm text-left rtl:text-right text-gray-500">
                 <tbody>
                     {todos.map((todo, index) => (
-                        <tr key={index} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 pl-6 py-4">
+                        <tr key={index} className="odd:bg-white even:bg-gray-50 border-b pl-6 py-4">
                             <td className="flex justify-between items-center">
                                 <input
                                     type="checkbox"
@@ -60,7 +60,7 @@ const TodoList = () => {
                                     onChange={() => handleToggleComplete(index)}
                                 />
                                 <span className={`mr-auto px-4 ${todo.completed ? 'line-through' : ''}`} style={{ wordBreak: 'break-all' }}>{todo.task}</span>
-                                <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onClick={() => handleRemoveTodo(index)}>Remove</button>
+                                <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5" onClick={() => handleRemoveTodo(index)}>Remove</button>
                             </td>
                         </tr>
                     ))}
